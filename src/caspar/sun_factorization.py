942581fb40b9c5f1a61539860ccc570691864810
Srilekha <srilekhag8@gmail.com>
2025-03-12 16:39:54 -0400

    updated the files to comply with autograd

--- src/caspar/sun_factorization.py
+++ src/caspar/sun_factorization.py
@@ -1,20 +1,21 @@
-#!/usr/bin/python                                                                  
-# -*- coding: utf-8 -*-                                                            
-#                                                                                  
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
 # sun_factorization.py: Functions to perform factorization of SU(n)
 #                       transformations into SU(2) transformations using
 #                       the method of arXiv:1708.00735.
-#                                                                                  
-# © 2017 Olivia Di Matteo (odimatte@uwaterloo.ca)                                  
-#                                                                                  
+#
+# © 2017 Olivia Di Matteo (odimatte@uwaterloo.ca)
+#
 # This file is part of the project Caspar.
-# Licensed under BSD-3-Clause                                                      
-#  
+# Licensed under BSD-3-Clause
+#
 
 import numpy as np
+import autograd.numpy as anp
 
 def su2_parameters(U):
-    """ Given a matrix in SU(2), parametrized as 
+    """ Given a matrix in SU(2), parametrized as
             U(a, b, g) = [ e^(i(a+g)/2) cos(b/2)  -e^(i(a-g)/2) sin(b/2)
                            e^(-i(a-g)/2) sin(b/2)  e^(-i(a+g)/2) cos(b/2) ]
         compute and return the parameters [a, b, g].
@@ -22,7 +23,7 @@ def su2_parameters(U):
     if U.shape != (2, 2):
         print("Error, matrix dimensions of su2_parameters must be 2x2.")
         return
-    if not np.isclose(np.linalg.det(U), 1):
+    if not anp.isclose(anp.linalg.det(U), 1):
         print("Error, matrix must have determinant 1 to be decomposed into SU(2) parameters.")
         return
 
@@ -30,26 +31,26 @@ def su2_parameters(U):
     # 1 and slightly above, when it should be 1 exactly. Isolate these cases
     # to prevent us from getting NaN.
     b = None
-    if np.isclose(np.absolute(U[0, 1]), 1):
-        b = 2 * np.arcsin(1)
+    if anp.isclose(anp.absolute(U[0, 1]), 1):
+        b = 2 * anp.arcsin(1)
     else:
-        b = 2 * np.arcsin(np.absolute(U[0, 1]))
+        b = 2 * anp.arcsin(anp.absolute(U[0, 1]))
 
-    arg_pos = np.angle(U[0, 0]) #(a + g)/2
-    arg_neg = -np.angle(U[1, 0]) #(a - g)/2
+    arg_pos = anp.angle(U[0, 0]) #(a + g)/2
+    arg_neg = -anp.angle(U[1, 0]) #(a - g)/2
     a, g = arg_pos + arg_neg, arg_pos - arg_neg
     return [a, b, g]
 
 
 def su3_parameters(U):
-    """ Uses the factorization of 
+    """ Uses the factorization of
             Rowe et. al (1999), "Representations of the Weyl group and Wigner
             functions for SU(3)", J. Math. Phys. 40 (7), 3604.
         to factorize an SU(3) transformation into 3 SU(2) transformations.
 
         Parameters for each SU(2) transformation are returned as a list
         [a, b, g] (three-parameter transformation) or [a, b, a] (two-parameter
-        transformation) where the matrices are to be parametrized as 
+        transformation) where the matrices are to be parametrized as
             SU_ij(a, b, g) = [ e^(i(a+g)/2) cos(b/2)  -e^(i(a-g)/2) sin(b/2)
                                e^(-i(a-g)/2) sin(b/2)  e^(-i(a+g)/2) cos(b/2) ]
 
@@ -59,10 +60,10 @@ def su3_parameters(U):
         The resultant matrix is expressed as
             U = SU_23(a1, b1, g1) SU_12(a2, b2, a2) SU_23(a3, b3, g3).
     """
-    if U.shape != (3, 3):                                                       
-        print("Error, matrix dimensions of su3_parameters must be 3x3.")        
+    if U.shape != (3, 3):
+        print("Error, matrix dimensions of su3_parameters must be 3x3.")
         return
-    if not np.isclose(np.linalg.det(U), 1):                                     
+    if not anp.isclose(anp.linalg.det(U), 1):
         print("Error, matrix must have determinant 1 to be decomposed into SU(2) parameters.")
         return
 
@@ -72,16 +73,16 @@ def su3_parameters(U):
     # Special case: if the top left element is 1, then we essentially
     # already have an SU(2) transformation embedded in an SU(3) transform,
     # so all we need to do is get the parameters of that SU(2) transform.
-    if np.isclose(x, 1):
+    if anp.isclose(x, 1):
         return [[0., 0., 0.], [0., 0., 0.], su2_parameters(U[1:,1:])]
-    # Another special case: the modulus of the top left element is 1. 
-    # Then we need to do a transformation on modes 1 and 2 to make the top 
+    # Another special case: the modulus of the top left element is 1.
+    # Then we need to do a transformation on modes 1 and 2 to make the top
     # entry 1, then an SU(2) transformation on modes 2 and 3 with what's left.
-    elif np.isclose(np.abs(x), 1):
+    elif anp.isclose(anp.abs(x), 1):
         # Compute the required phase matrix and embed into SU(3)
-        phase_su2 = np.matrix([[np.conj(x), 0], [0, x]])
+        phase_su2 = anp.matrix([[anp.conj(x), 0], [0, x]])
 
-        full_phase_su2 = np.asmatrix(np.identity(3)) + 0j
+        full_phase_su2 = anp.asmatrix(anp.identity(3)) + 0j
         full_phase_su2[0:2, 0:2] = phase_su2
 
         # Compute what's left of the product, and the parameters
@@ -91,25 +92,25 @@ def su3_parameters(U):
         return [[0., 0., 0.], su2_parameters(phase_su2.getH()), su2_parameters(remainder_su2)]
 
     # Typical case
-    cf = np.sqrt(1 - pow(np.absolute(x), 2))
-    capY, capZ = y / cf, z / cf 
+    cf = anp.sqrt(1 - pow(anp.absolute(x), 2))
+    capY, capZ = y / cf, z / cf
 
     # Build the SU(2) transformation matrices
     # SU_23(3) - three parameters
-    left = np.matrix([[1, 0, 0], 
-                      [0, capY, -np.conj(capZ)],
-                      [0, capZ, np.conj(capY)]])
+    left = anp.matrix([[1, 0, 0],
+                      [0, capY, -anp.conj(capZ)],
+                      [0, capZ, anp.conj(capY)]])
     left_params = su2_parameters(left[1:, 1:])
 
     # SU_12(2) - only two parameters
-    middle = np.matrix([[x, -cf, 0],
-                        [cf, np.conj(x), 0],
+    middle = anp.matrix([[x, -cf, 0],
+                        [cf, anp.conj(x), 0],
                         [0, 0, 1]])
     middle_params = su2_parameters(middle[0:2, 0:2])
 
     # SU_23(3) - again three parameters
     right = middle.getH() * left.getH() * U
-    right_params = su2_parameters(right[1:, 1:]) 
+    right_params = su2_parameters(right[1:, 1:])
 
     return [left_params, middle_params, right_params]
 
@@ -131,22 +132,22 @@ def build_staircase(U):
 
     # There are a number of special cases to consider which occur when the
     # left-most column contains all 0s except for one entry.
-    moduli = [np.abs(U[x, 0]) for x in range(n)]
-    if np.allclose(sorted(moduli), [0.] * (n - 1) + [1]): 
+    moduli = [anp.abs(U[x, 0]) for x in range(n)]
+    if anp.allclose(sorted(moduli), [0.] * (n - 1) + [1]):
         # In the special case where the top-most entry is a 1, or within some
         # small tolerance of it, we basically already have an SU(n-1) transformation
-        # in there so just fill with empty parameters 
-        if np.isclose(running_prod[0, 0], 1):
+        # in there so just fill with empty parameters
+        if anp.isclose(running_prod[0, 0], 1):
             transformations = [[0., 0., 0.]] * (n - 1)
         # Another special case is when the top left entry has modulus 1 (or close
-        # to it). Now we need to add a separate phase shift as well. 
-        elif np.isclose(np.abs(running_prod[0, 0]), 1):
+        # to it). Now we need to add a separate phase shift as well.
+        elif anp.isclose(anp.abs(running_prod[0, 0]), 1):
             # "Phase shift" by applying an SU(2) transformation to cancel out the
             # top-most phase. Do nothing to everything else.
-            phase_su2 = np.matrix([[np.conj(running_prod[0, 0]), 0], [0, running_prod[0, 0]]])
+            phase_su2 = anp.matrix([[anp.conj(running_prod[0, 0]), 0], [0, running_prod[0, 0]]])
             transformations = [[0., 0., 0.]] * (n - 2) + [su2_parameters(phase_su2.getH())]
 
-            full_phase_su2 = np.asmatrix(np.identity(n)) + 0j
+            full_phase_su2 = anp.asmatrix(anp.identity(n)) + 0j
             full_phase_su2[0:2, 0:2] = phase_su2
             running_prod = full_phase_su2 * running_prod
         else:
@@ -154,21 +155,21 @@ def build_staircase(U):
             # reaches the top and then apply a phase transformation.
             for rot_idx in range(n - 1, 0, -1):
                 if running_prod[rot_idx, 0] != 0:
-                    permmat = np.matrix([[0, -1], [1, 0]])
-                
-                    full_permmat = np.asmatrix(np.identity(n)) + 0j
+                    permmat = anp.matrix([[0, -1], [1, 0]])
+
+                    full_permmat = anp.asmatrix(anp.identity(n)) + 0j
                     full_permmat[rot_idx-1:rot_idx+1, rot_idx-1:rot_idx+1] = permmat
                     temp_product = full_permmat * running_prod
 
                     if rot_idx == 1: # If we're at the top, add the phase too
-                        phase_su2 = np.matrix([[np.conj(temp_product[0, 0]), 0], [0, temp_product[0, 0]]])
+                        phase_su2 = anp.matrix([[anp.conj(temp_product[0, 0]), 0], [0, temp_product[0, 0]]])
                         permmat = phase_su2 * permmat
 
                     transformations.append(su2_parameters(permmat.getH()))
 
-                    full_trans = np.asmatrix(np.identity(n)) + 0j
-                    full_trans[rot_idx-1:rot_idx+1, rot_idx-1:rot_idx+1] = permmat 
-                        
+                    full_trans = anp.asmatrix(anp.identity(n)) + 0j
+                    full_trans[rot_idx-1:rot_idx+1, rot_idx-1:rot_idx+1] = permmat
+
                     running_prod = full_trans * running_prod
 
                 else: # Otherwise do nothing between these modes
@@ -180,29 +181,29 @@ def build_staircase(U):
 
             # Initially we work with the inverses in order to "0 out" entries
             # from the left; later we'll get the parameters from the "true" matrices.
-            Rij_inv = np.asmatrix(np.identity(2)) + 0j
-            full_Rij_inv = np.asmatrix(np.identity(n)) + 0j
+            Rij_inv = anp.asmatrix(anp.identity(2)) + 0j
+            full_Rij_inv = anp.asmatrix(anp.identity(n)) + 0j
 
             if rot_idx != n - 2:
                 # The denominator of the transformation is the difference of
                 # absolute values of all columns *up* to this point.
                 sum_of_column = 0
                 for k in range(i):
-                    sum_of_column += pow(np.absolute(running_prod[k,0]), 2)
-                cf = np.sqrt(1 - sum_of_column)
+                    sum_of_column += pow(anp.absolute(running_prod[k,0]), 2)
+                cf = anp.sqrt(1 - sum_of_column)
 
-                y, z = running_prod[i, 0], running_prod[j, 0] 
-                capY, capZ  = y / cf, z / cf 
+                y, z = running_prod[i, 0], running_prod[j, 0]
+                capY, capZ  = y / cf, z / cf
 
                 # Build the SU(2) transformation and embed it into the larger matrix
-                Rij_inv = np.matrix([[np.conj(capY), np.conj(capZ)], 
+                Rij_inv = anp.matrix([[anp.conj(capY), anp.conj(capZ)],
                                      [-capZ, capY]])
-            else: 
+            else:
                 # The last transformation, R12 is special and the rotation has
                 # a different form
                 x = U[0,0]
-                cf = np.sqrt(1 - pow(np.absolute(x), 2))
-                Rij_inv = np.matrix([[np.conj(x), cf], 
+                cf = anp.sqrt(1 - pow(anp.absolute(x), 2))
+                Rij_inv = anp.matrix([[anp.conj(x), cf],
                                      [-cf, x]])
 
             # Add the transformation to the sequence and update the product
@@ -219,10 +220,10 @@ def build_staircase(U):
 def sun_parameters(U):
     """ Compute the set of parameters of the SU(2) transforms in the
         factorization scheme.
-        
-        This is a recursive process. The first step is to produce a 
+
+        This is a recursive process. The first step is to produce a
         "staircase" of transformations on adjacent modes (d-1, d), (d-2, d-1),...
-        so that what's left is an SU(n-1) transformation embedded in the lower 
+        so that what's left is an SU(n-1) transformation embedded in the lower
         portion of the original system. This is performed recursively down to
         the case of SU(3) where we use the Rowe et al algorithm to get the
         rest of the transformation.
@@ -235,14 +236,14 @@ def sun_parameters(U):
         # process on the resultant SU(n-1) transformation
         staircase_transformation, new_U = build_staircase(U)
         Unm1 = new_U[1:,1:] # Grab the lower chunk of the matrix
-        return staircase_transformation + sun_parameters(Unm1) 
+        return staircase_transformation + sun_parameters(Unm1)
 
 
 def sun_factorization(U):
-    """ Decompose an arbitrary element in SU(n) as a sequence of 
-        SU(2) transformations as per our method arXiv:1708.00735. 
+    """ Decompose an arbitrary element in SU(n) as a sequence of
+        SU(2) transformations as per our method arXiv:1708.00735.
 
-        Takes as input a numpy matrix U.
+        Takes as ianput a numpy matrix U.
         Returns a list of operations in the form "i,i+1", [a, b, g] where
         the i indicate the modes of an SU(2) transformation and [a, b, g]
         are its parameters.
@@ -254,11 +255,11 @@ def sun_factorization(U):
     if n < 3:
         print("Error, matrix for decomposition must be at least 3x3.")
 
-    if not np.isclose(np.linalg.det(U), 1):                           
+    if not anp.isclose(anp.linalg.det(U), 1):
         print("Error, matrix must have determinant 1.")
         return None
 
-    if not np.allclose(U * U.getH(), np.eye(n)):
+    if not anp.allclose(U * U.getH(), anp.eye(n)):
         print("Error, matrix must be unitary with determinant 1.")
         return None
 
@@ -267,11 +268,11 @@ def sun_factorization(U):
 
     # Add the info about which modes each transformation is on
     parameters = []
-    param_idx = 0                                                                   
+    param_idx = 0
 
-    for md2 in range(2, n + 1):                                                 
-        for md1 in range(n - 1, md2 - 2, -1):                               
+    for md2 in range(2, n + 1):
+        for md1 in range(n - 1, md2 - 2, -1):
             parameters.append((str(md1) + "," + str(md1+1), parameters_no_modes[param_idx]))
-            param_idx += 1  
+            param_idx += 1
 
     return parameters
--- src/caspar/sun_reconstruction.py
+++ src/caspar/sun_reconstruction.py
@@ -1,53 +1,67 @@
-#!/usr/bin/python                                                                  
-# -*- coding: utf-8 -*-                                                            
-#                                                                                  
-# sun_reconstruction.py: Functions to perform reconstruction of SU(n)                
-#                        transformations using a list of modes and parameters. 
-#                                                                                  
-# © 2017 Olivia Di Matteo (odimatte@uwaterloo.ca)                                  
-#                                                                                  
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# sun_reconstruction.py: Functions to perform reconstruction of SU(n)
+#                        transformations using a list of modes and parameters.
+#
+# © 2017 Olivia Di Matteo (odimatte@uwaterloo.ca)
+#
 # This file is part of the project Caspar.
-# Licensed under BSD-3-Clause                                                      
-#              
+# Licensed under BSD-3-Clause
+#
 
-import numpy as np                                                                 
-                                                                                   
-def embed_su2(n, i, j, params): 
-    """ Embed the SU(2) transformation given by params into modes i and j       
-        of an SU(n) matrix                                                         
-            SU_ij(3) = [ e^(i(a+g)/2) cos(b/2)   -e^(i(a-g)/2) sin(b/2) 
+import numpy as np
+import autograd.numpy as anp
+
+def embed_su2(n, i, j, params):
+    """ Embed the SU(2) transformation given by params into modes i and j
+        of an SU(n) matrix
+            SU_ij(3) = [ e^(i(a+g)/2) cos(b/2)   -e^(i(a-g)/2) sin(b/2)
                         e^(-i(a-g)/2) sin(b/2)   e^(-i(a-g)/2) cos(b/2) ]
 
         Returns the full n-dimensional matrix.
-    """                                                                            
-    a, b, g = params[0], params[1], params[2]                                      
-                                                                                   
+    """
+    a, b, g = params[0], params[1], params[2]
+
     # Create SU(2) element and scaled by loss if desired.
-    Rij = np.array([[np.exp(1j*(a+g)/2)*np.cos(b/2), -np.exp(1j*(a-g)/2)*np.sin(b/2)],
-                    [np.exp(-1j*(a-g)/2)*np.sin(b/2), np.exp(-1j*(a+g)/2)*np.cos(b/2)]])       
-                         
+    Rij = anp.array([[anp.exp(1j*(a+g)/2)*anp.cos(b/2), -anp.exp(1j*(a-g)/2)*anp.sin(b/2)],
+                    [anp.exp(-1j*(a-g)/2)*anp.sin(b/2), anp.exp(-1j*(a+g)/2)*anp.cos(b/2)]])
+
     # Stuff it into modes i and j of SU(n)
-    full_Rij = np.asmatrix(np.identity(n)) + 0j                                    
-    full_Rij[i:j+1, i:j+1] = Rij
+    # full_Rij = anp.asmatrix(anp.identity(n)) + 0j
+    # full_Rij[i:j+1, i:j+1] = Rij
+
+    full_Rij = anp.identity(n, dtype=complex)  # Use anp.array() instead of asmatrix()
+
+    # Manually replace the submatrix without in-place modification
+    full_Rij = anp.concatenate([
+        full_Rij[:i, :],  # Keep the top part
+        anp.concatenate([
+            full_Rij[i:j+1, :i],  # Keep the left part of the submatrix
+            Rij,                  # Replace the submatrix
+            full_Rij[i:j+1, j+1:]  # Keep the right part of the submatrix
+        ], axis=1),
+        full_Rij[j+1:, :]  # Keep the bottom part
+    ], axis=0)
 
     return full_Rij
 
 
 def sun_reconstruction(n, parameters):
-    """ Reconstruct an SU(n) matrix using a list of transformations given as 
+    """ Reconstruct an SU(n) matrix using a list of transformations given as
         tuples ("i,j", [a, b, g]). In theory any sequence can be given, but
-        it would make most sense to use something of the form of the sequence 
+        it would make most sense to use something of the form of the sequence
         of n(n-1)/2 transformations given by sun_factorization algorithm.
 
-        Note: the matrix is put together "backwards" as compared to the 
-        circuit diagram, i.e. we multiply on the right always by the next 
+        Note: the matrix is put together "backwards" as compared to the
+        circuit diagram, i.e. we multiply on the right always by the next
         matrix to get U, rather than on the left. In the end it really
-        doesn't matter since the decomposition itself is symmetric, as long 
+        doesn't matter since the decomposition itself is symmetric, as long
         as everything is consistent.
     """
 
     # Hold the result
-    U = np.asmatrix(np.identity(n)) + 0j
+    U = anp.array(anp.identity(n)) + 0j
 
     for param in parameters:
         # Get the indices of the modes
@@ -57,7 +71,7 @@ def sun_reconstruction(n, parameters):
         if md1 not in range(n) or md2 not in range(n):
             print("Mode combination " + str(md1 + 1) + "," + str(md2 + 1) + \
                     " is invalid for a system of dimension " + str(n) + ".")
-            return 
+            return
 
         if md2 != md1 + 1:
             print("Mode combination " + str(md1 + 1) + "," + str(md2 + 1) + " is invalid. ")
@@ -65,7 +79,32 @@ def sun_reconstruction(n, parameters):
             return
 
         # Compute the next transformation and multiply
-        next_trans = embed_su2(n, md1, md2, param[1])
-        U = U * next_trans
+        next_trans = embed_su2(n, md1, md2, anp.array(param[1])) # removed anp.array() for RHS
+        U = U @ next_trans
+
+    return U
+
+
+def sun_reconstruction_mod(n, parameters, breakpoints):
+    # Hold the result
+    U = anp.array(anp.identity(n)) + 0j
+
+    for param, brk in zip(parameters, breakpoints):
+        # Get the indices of the modes
+        md1, md2 = param[0] - 1, param[1] - 1
+
+        if md1 not in range(n) or md2 not in range(n):
+            print("Mode combination " + str(md1 + 1) + "," + str(md2 + 1) + \
+                    " is invalid for a system of dimension " + str(n) + ".")
+            return
+
+        if md2 != md1 + 1:
+            print("Mode combination " + str(md1 + 1) + "," + str(md2 + 1) + " is invalid. ")
+            print("Currently only transformations on adjacent modes are implemented.")
+            return
+
+        # Compute the next transformation and multiply
+        next_trans = anp.array(embed_su2(n, md1, md2, anp.array(brk))) # removed anp.array() for RHS
+        U = U @ next_trans
 
     return U
